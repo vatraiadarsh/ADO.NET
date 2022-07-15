3ef1c2db341daeded92a1af3fcc9486821f7ef45
Adarsha <vatraiadarsh@gmail.com>
2022-07-15 20:16:44 +1000

    db connection

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ConsoleApp
+﻿using MySql.Data.MySqlClient;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MySqlConnection connection = null;
+
+            try
+            {
+                connection = new MySqlConnection();
+                connection.ConnectionString = "Server=localhost;Database=apple;Uid=root;Pwd=''";
+                connection.Open();
+                Console.WriteLine("DB connected");
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
