64f30b8eb900e2b457a92043432f090240253a77
Adarsha <vatraiadarsh@gmail.com>
2022-07-15 21:04:42 +1000

    Skeletonapp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace ConsoleApp
 {
@@ -11,9 +12,52 @@ namespace ConsoleApp
             try
             {
                 connection = new MySqlConnection();
-                connection.ConnectionString = "Server=localhost;Database=apple;Uid=root;Pwd=''";
+                connection.ConnectionString = "Server=localhost;Database=ado;Uid=root;Pwd=''";
                 connection.Open();
                 Console.WriteLine("DB connected");
+
+                while (true)
+                {
+                    Console.WriteLine("Username");
+                    String username = Console.ReadLine();
+                    Console.WriteLine("Password");
+                    String password = Console.ReadLine();
+
+                    string sql = "select * from users where username=@username and password = @password ";
+
+                    MySqlCommand SqlCommand = new MySqlCommand();
+                    SqlCommand.CommandText = sql;
+                    SqlCommand.CommandType = CommandType.Text;
+                    SqlCommand.Connection = connection;
+
+                    SqlCommand.Parameters.Add(new MySqlParameter()
+                    {
+                        DbType = DbType.AnsiString,
+                        ParameterName = "username",
+                        Value = username,
+                    });
+
+                    SqlCommand.Parameters.Add(new MySqlParameter()
+                    {
+                          
+                        DbType = DbType.AnsiString,
+                        ParameterName = "Password",
+                        Value = password,
+                    });
+
+                    using (MySqlDataReader reader = SqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["username"]);
+                            Console.WriteLine(reader["password"]);
+                            Console.WriteLine(reader["status"]);
+                            Console.WriteLine("------------------------------------------");
+                        }
+                        connection.Close();
+                    }
+                }
+
             } catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
---
+++ db.sql
@@ -0,0 +1,16 @@
+﻿drop database ado;
+create database ado;
+use ado;
+
+create table users(
+username varchar(225),
+email varchar(225),
+password varchar (225),
+status tinyint default 0
+);
+
+
+insert into users(username,email,password) values ("Adarsh","adarsh@gmail.com","adarsh");
+insert into users(username,email,password,status) values ("test","test@gmail.com","test",1);
+
+select * from users;
